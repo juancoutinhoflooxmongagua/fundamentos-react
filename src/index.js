73e1bda7432e8a607aeb32e5fd302b05fceefad2
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-25 11:04:29 -0300

    feat: first component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+import './index.css'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
+import First from './components/basics/first.js'
 
 const el = document.getElementById('root')
 const root = createRoot(el)
 
-const tag =      <i>Hello World</i>
+const tag = <i>Hello World</i>
 
 root.render(
     <h1>
+        <First></First>
+        <br></br>
         { tag }
     </h1>
 )
