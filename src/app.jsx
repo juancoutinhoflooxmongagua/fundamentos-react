88fd37ed803726d55f5d559bf05a1cc0a81c9ed9
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-25 12:17:42 -0300

    feat: add Card for Project

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,7 @@
 import './index.css'
+
 import React from 'react'
+import Card from './components/layout/card'
 import First from './components/basics/first'
 import Parameters from './components/basics/parameters'
 import Frags from './components/basics/frags'
@@ -9,6 +11,7 @@ export default _ =>
 
     <div id="app">
         <h1>
+            <Card></Card>
             <First />
             <br />
             <Parameters titulo="Eu sou o titulo" />
--- src/random.jsx
+++ src/random.jsx
@@ -7,6 +7,7 @@ let random_number = Math.floor(Math.random() * (max - min +1))
 export default function Frags(props) {
     return (
         <>
+            <h1>Número Aleatório do dia.</h1>
             <p>{ random_number }</p>
         </>
     )}
